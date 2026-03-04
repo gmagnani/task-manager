8d6aa2068ee68966de0fece22948ab9d4098b8f6
gmagnani <magnanigeovanni@gmail.com>
2026-03-04 11:42:06 -0300

    feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,21 +1,43 @@
+import CheckIcon from '../assets/icons/check.svg?react'
+import LoaderIcon from '../assets/icons/loader.svg?react'
+import DetailIcon from '../assets/icons/details.svg?react'
+
 const TaskItem = ({ task }) => {
-    const getStatusClasses = () => {
-        switch (task.status) {
-            case 'pending':
-                return 'bg-[#35383e]/10 text-[#35383e]'
-            case 'in_progress':
-                return 'bg-[#ffaa04]/10 text-[#ffaa04]'
-            case 'done':
-                return 'bg-[#00adb5]/10 text-[#00adb5]'
-            default:
-                return ''
-        }
+  const getStatusClasses = () => {
+    switch (task.status) {
+      case 'pending':
+        return 'bg-[#35383e]/10 text-[#35383e]'
+      case 'in_progress':
+        return 'bg-[#ffaa04]/10 text-[#ffaa04]'
+      case 'done':
+        return 'bg-[#00adb5]/10 text-[#00adb5]'
+      default:
+        return ''
     }
-    return(
-        <div className={`px-4 py-3 flex items-center text-sm gap-2 rounded-lg ${getStatusClasses()}`}>
-            {task.title}
-        </div>
-    )
+  }
+  return (
+    <div
+      className={`px-4 py-3 justify-between flex items-center text-sm gap-2 rounded-lg ${getStatusClasses()}`}
+    >
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+              checked={task.status === 'done'}
+            className="absolute opacity-0 w-full h-full cursor-pointer"
+          />
+          {task.status === 'done' && <CheckIcon />}
+          {task.status === 'in_progress' && <LoaderIcon className="animate-spin" />}
+        </label>
+        {task.title}
+      </div>
+      <a href="#" className="text-[#00adb5] hover:text-[#00adb5]/80">
+        <DetailIcon />
+      </a>
+    </div>
+  )
 }
 
-export default TaskItem;
+export default TaskItem
