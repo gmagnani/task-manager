b3bc8825ff9ff13022f58c5d889ec82263b49df4
gmagnani <magnanigeovanni@gmail.com>
2026-03-13 16:56:19 -0300

    feat: refactor API calls in task management hooks to use centralized axios instance; improve code consistency and maintainability

--- src/hooks/data/use-add-task.js
+++ src/hooks/data/use-add-task.js
@@ -1,15 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import axios from 'axios'
+import { api } from '../../lib/axios'
 
 export const useAddTask = () => {
   const queryClient = useQueryClient()
   return useMutation({
     mutationKey: 'addTask',
     mutationFn: async (newTask) => {
-      const { data: createdTask } = await axios.post(
-        'http://localhost:3000/tasks',
-        newTask
-      )
+      const { data: createdTask } = await api.post('/tasks', newTask)
 
       return createdTask
     },
